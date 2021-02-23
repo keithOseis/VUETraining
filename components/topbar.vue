802564a706178e8131e500334cdfce82477cb927
keithOseis <78473033+keithOseis@users.noreply.github.com>
2021-02-23 14:27:52 +0100

    init

---
+++ components/topbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <v-app-bar color="lightblue" dense flat>
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
---
+++ layout/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <topbar/>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import Topbar from "~/components/topbar.vue";
+
+export default {
+  components: {
+    topbar: Topbar,
+  },
+};
+</script>
